303ece2817e5ecf45833607731ac66eb8f94c31b
Hazdryx <hazdryx@gmail.com>
2021-09-04 11:10:11 -0500

    Created a simple program using commander.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 #! /usr/bin/env node
 
-console.log('hello!');
+// Setup
+const { program } = require('commander');
+program
+  .name('ciao')
+  .requiredOption('-w, --ws <name>', 'the workspace to be used (required)')
+  .version('0.1.0');
+
+// Parse
+program.parse(process.argv);
+
+// Run
+console.log(program.opts().ws);
