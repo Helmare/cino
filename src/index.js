92b46b1021e8b82a6a4222064fa2785d3cdc7b46
Hazdryx <hazdryx@gmail.com>
2021-09-05 11:35:06 -0500

    added dir command for piping

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 #! /usr/bin/env node
+const { WORKSPACE_DIR } = require('./core/workspace');
 
 // Setup
 const { program } = require('commander');
 program
   .name('ciao')
-  .requiredOption('-w, --ws <name>', 'the workspace to be used (required)')
+  .option('-w, --ws <name>', 'the workspace to be used (required)')
   .version('0.1.0');
+
+program.command('dir').action(() => {
+  console.log(WORKSPACE_DIR);
+});
 
 // Commands
 require('./commands/create');
