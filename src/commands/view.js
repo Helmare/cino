be68757ff149cc64418cab92c78b33ffffdaff47
Hazdryx <hazdryx@gmail.com>
2021-09-19 09:16:04 -0500

    updated view command with range defaults

--- src/commands/view.js
+++ src/commands/view.js
@@ -7,13 +7,31 @@ const cmd = buildWorkspaceCommand({
   description: 'displays a workspace',
   aliases: ['v'],
   action(ws) {
-    console.log('\n---------------------------------------');
-    console.log(`| ${chalk.bold('name'.padStart(11))} | ${chalk.bold(ws.name.padEnd(21))} |`);
-    console.log(`| ${chalk.bold('time'.padStart(11))} | ${chalk.cyanBright(`${ws.time.toFixed(2)} hours`.padEnd(21))} |`);
-    console.log('| ----------------------------------- |');
-    console.log(`|      ${chalk.bold('in')}     |     ${chalk.bold('out')}     |  ${chalk.bold('hours')}  |`);
-    console.log('| ----------- | ----------- | ------- |');
-    ws.cycles.reverse().forEach(cyc => {
+    // Setup
+    let start = new Date();
+    start.setHours(0, 0, 0, 0);
+    start.setDate(start.getDate() - start.getDay());
+
+    let end = new Date(start.getTime());
+    end.setDate(end.getDate() + 6);
+    end.setHours(23, 59, 59);
+
+    const view = ws.view(start, end);
+    
+
+    // Header
+    console.log('\n-----------------------------------------');
+    console.log(`| ${chalk.bold('name'.padStart(11))} | ${chalk.bold(ws.name.padEnd(23))} |`);
+    console.log(`| ${chalk.bold('all hours'.padStart(11))} | ${chalk.cyanBright(`${ws.time.toFixed(2)} hours`.padEnd(23))} |`);
+    console.log(`| ${chalk.gray('-----------')} | ${chalk.gray('-----------------------')} |`)
+    console.log(`| ${chalk.bold('range'.padStart(11))} | ${chalk.greenBright(format('MM/dd/yyyy', view.start))} - ${chalk.greenBright(format('MM/dd/yyyy', view.end))} |`);
+    console.log(`| ${chalk.bold('hours'.padStart(11))} | ${chalk.cyanBright(`${view.time.toFixed(2)} hours`.padEnd(23))} |`);
+    console.log('| ------------------------------------- |');
+    console.log(`|      ${chalk.bold('in')}     |     ${chalk.bold('out')}     |   ${chalk.bold('hours')}   |`);
+    console.log('| ----------- | ----------- | --------- |');
+
+    // Cycles
+    view.cycles.forEach(cyc => {
       let str = `${chalk.greenBright(format('MM/dd hh:mm', cyc.in))} | `;
       if (cyc.out) {
         str += `${chalk.greenBright(format('MM/dd hh:mm', cyc.out))}`;
@@ -21,9 +39,11 @@ const cmd = buildWorkspaceCommand({
       else {
         str += '     -     ';
       }
-      console.log(`| ${str} | ${chalk.cyanBright(cyc.duration.toFixed(2).padStart(7))} |`);
+      console.log(`| ${str} | ${chalk.cyanBright(cyc.duration.toFixed(2).padStart(9))} |`);
     });
-    console.log('---------------------------------------\n');
+
+    // Footer
+    console.log('-----------------------------------------\n');
   }
 });
 
